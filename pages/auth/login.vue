94d7895c51a081d476b0795f2072c2a69c3727de
saira <saira@ceribs.com>
2020-11-08 22:01:10 +0530

    form now displays errors

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,47 +1,80 @@
 <template>
-     <section class="authentication">
-                <div class="auth-body">
-                    <h1 class="text-uppercase fw-500 mb-4 text-center font-22">
-                        Login
-                    </h1>
-                    <form class="auth-form" action="" method="">
-                        <div class="form-group">
-                            <input
-                                type="text"
-                                name="email"
-                                class="form-control form-control-lg font-14 fw-300"
-                                placeholder="Email"
-                            />
-                        </div>
-                        <div class="form-group">
-                            <input
-                                type="password"
-                                name="password"
-                                class="form-control form-control-lg font-14 fw-300"
-                                placeholder="Password"
-                            />
-                        </div>
-                        <div class="mt-4 mb-4 clearfix">
-                            <a class="forgot-pass color-blue font-14 fw-400" href="#"> Forgot password? </a>
-                        </div>
-                        <div class="text-right">
-                            <button type="submit" class="btn btn-primary primary-bg-color font-16 fw-500 text-uppercase">
-                                Login
-                            </button>
-                        </div>
-                        <p class="font-14 fw-400 text-center mt-4">
-                            Don't have an account yet?
-                            <a class="color-blue" href="#"> Create an account</a>
-                        </p>
-                    </form>
+    <section class="authentication">
+        <div class="auth-body">
+            <h1 class="text-uppercase fw-500 mb-4 text-center font-22">
+                Login
+            </h1>
+            <form class="auth-form" @submit.prevent="submit">
+                <alert-error v-if="form.errors.has('verification')" :form="form">
+                    {{form.errors.get('verification')}}
+                    <nuxt-link :to="{name: 'verification.resend'}" >Resend verification email</nuxt-link>
+                </alert-error>
+                <div class="form-group">
+                    <input
+                        type="text"
+                        v-model.trim="form.email"
+                        name="email"
+                        class="form-control form-control-lg font-14 fw-300"
+                        :class="{'is-invalid' : form.errors.has('email')}"
+                        placeholder="Email"
+                    />
+                    <has-error :form="form" field="email"></has-error>
                 </div>
-            </section>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        v-model.trim="form.password"
+                        name="password"
+                        class="form-control form-control-lg font-14 fw-300"
+                        :class="{'is-invalid' : form.errors.has('password')}"
+                        placeholder="Password"
+                    />
+                    <has-error :form="form" field="password"></has-error>
+
+                </div>
+                <div class="mt-4 mb-4 clearfix">
+                    <nuxt-link to="/password/email" class="forgot-pass color-blue font-14 fw-400"> Forgot password? </nuxt-link>
+                </div>
+                    <div class="text-right">
+                    <button type="submit" :disabled="form.busy" class="btn btn-primary primary-bg-color font-16 fw-500 text-uppercase">
+                        <span v-if="form.busy">
+                            <i class ="fas fa-spinner fa-spin"></i>
+                        </span>
+                        Login
+                    </button>
+                </div>
+                <p class="font-14 fw-400 text-center mt-4">
+                    Don't have an account yet?
+                    <nuxt-link :to="{name: 'register'}" class="color-blue" > Create an account</nuxt-link>
+                </p>
+            </form>
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
-    
-}
+    data(){
+        return {
+            form: this.$vform({
+                email:'',
+                password:''
+            })
+        }
+    },
+
+    methods:{
+        submit(){
+            this.$auth.loginWith('local',{ data:this.form})
+            .then(res => {
+                console.log(res);
+            })
+            .catch(e => {
+                this.form.errors.set(e.response.data.errors);
+            });
+        }
+    }    
+};
 </script>
 
 <style>
