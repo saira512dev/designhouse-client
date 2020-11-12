d2da9b35bbe940046af678536caccd056aab71d1
saira <saira@ceribs.com>
2020-11-12 16:17:56 +0530

    props for readonly option added

--- components/_global/inputs/_base-input.vue
+++ components/_global/inputs/_base-input.vue
@@ -35,6 +35,10 @@ export default {
             type: String,
             default:'text'
         },
+        readonly: {
+            type: Boolean,
+            default: false
+        }
     },
 
     methods: {
