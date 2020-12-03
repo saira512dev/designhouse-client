1b5c7914f5eaab955844eff2c61251a0d7c81a54
saira <saira@ceribs.com>
2020-12-03 12:40:28 +0530

    new plugin for gmap added

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [ '~plugins/vform',
-'~components/_global' ],
+'~components/_global',{ src: '~/plugins/gmaps', ssr: false } ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
