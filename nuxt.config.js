233a9f286ecaada1ed7af39d55ab821fa7d526ae
saira <saira@ceribs.com>
2021-01-25 12:22:51 +0530

    new component designcomment added

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   css: [ '@/assets/scss/main.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [ '~plugins/vform',
+  plugins: [ '~plugins/vform','~components/DesignComment',
 '~components/_global',{ src: '~/plugins/gmaps', ssr: false } ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
