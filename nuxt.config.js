5703879ad8efef1e4a4d900720adf752774c126e
saira <saira@ceribs.com>
2021-03-21 22:55:37 +0530

    title of client app changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'designhouse-client',
+    title: 'designhouse',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
